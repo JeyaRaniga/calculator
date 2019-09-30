fcbc71ce9c73d9f79102d4f2d976244cbf2c55a2
Jeya Raniga <34700984+JeyaRaniga@users.noreply.github.com>
2019-09-30 16:58:01 +1300

    added pseudocode

---
+++ calculator.js
@@ -0,0 +1,25 @@
+
+// getNumber function to get a number from [input]
+// declare input var
+// create a switch statement for numbers 0-9 [input].value += number
+
+
+// clearScreen function - when you click CE it should clear screen.
+// getElementById(input).value and getElementById(answer).value = ''
+
+
+// getOperator function
+// input var
+// create a switch statement for the operators for +,-,/, * [input] += operator
+
+// backSpace function
+// remove the last character in [input]
+
+// compute function
+//get [input]
+//define ans math.floor ( +eval(input.value))
+// get [answer] = "=" + ans
+
+// brackets function - the brackets must close.
+// var = i 
+// if i == 0 then '(' and i== 1 else if i== 1 then ')' and i==0
